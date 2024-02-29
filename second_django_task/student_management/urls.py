1919e8c6e3f53133311888c7c3c1cb327c857e15
fidyanhakim-technman <155768580+fidyanhakim-technman@users.noreply.github.com>
2024-02-29 09:28:10 -0500

    Third Task Done

---
+++ second_django_task/student_management/urls.py
@@ -0,0 +1,12 @@
+# student_management/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('standards/', views.StandardList.as_view()),
+    path('standards/<int:pk>/', views.StandardDetail.as_view()),
+    path('students/', views.StudentList.as_view()),
+    path('students/<int:pk>/', views.StudentDetail.as_view()),
+    path('studentsWithPageination/<int:page>/<int:per_page>', views.student_list, name='student_list'),
+]
